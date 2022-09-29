4974ebfdac07f367092b47d8e1d8740b78a7df98
Kutsuzawa <kutu.ryo1106@gmail.com>
2022-09-30 03:41:33 +0900

    RUを追加する部分を実装

--- ハミルトニアン(-有り)/ハイゼンベルク/Heisenberg.jl
+++ ハミルトニアン(-有り)/ハイゼンベルク/Heisenberg.jl
@@ -1,4 +1,6 @@
 using LinearAlgebra, Random, RandomMatrices
+using JLD2
+using FileIO
 #using Plots
 
 Igate = Matrix{ComplexF64}(I,2,2)                  #1-qubitの単位行列
@@ -66,11 +68,45 @@ function make_hamiltonian(Nq::Int)#ハイゼンベルグモデル
     return hamiltonian
 end
 
+function make_pauli(index::Int, Nq::Int, pauli_name)
+    pauli = zeros(ComplexF64,(2^Nq,2^Nq))
+    for i in 1:Nq
+        if i == 1 #一番最初のループはおおもとになる行列の宣言をする(pauliを作る)
+            if index == 1 #1st qubitがXかYのとき
+                if pauli_name == "X"
+                    pauli = Xgate
+                elseif pauli_name == "Y"
+                    pauli = Ygate
+                elseif pauli_name == "Z"
+                    pauli = Zgate
+                else
+                    println("ERROR: undefined!")
+                    return False
+                end
+            else
+                pauli = Igate
+            end
+        else #2ループ目以降は変数pauliがあるはずなので、pauli \tensor (X or Z or I)を計算してpauliを上書き代入
+            if i == index #XかYを代入するタイミングのとき
+                if pauli_name == "X"
+                    pauli = kron(pauli, Xgate)
+                elseif pauli_name == "Y"
+                    pauli = kron(pauli, Ygate)
+                elseif pauli_name == "Z"
+                    pauli = kron(pauli, Zgate)
+                end
+            else
+                pauli = kron(pauli, Igate)
+            end
+        end
+    end
+    return pauli
+end
 
-function make_unitary_pool(Nq::Int,S)
+function make_unitary_pool(Nq::Int,S,N)
     unitary_pool = zeros(ComplexF64,2^Nq,2^Nq,N)
 
-    for i in 1:length(S)
+    for i in 1:N #N
         hamiltonian_unitary = H_vec*diagm(exp.(-1.0im*H_val*S[i]))*H_vec'
         #hamiltonian_unitary = exp(-1.0im*H*S[i])
         if i == 1
@@ -107,39 +143,11 @@ function chose_unitary(unitary_pool,S,t)
     return unitary
 end
 
-function make_pauli(index::Int, Nq::Int, pauli_name)
-    pauli = zeros(ComplexF64,(2^Nq,2^Nq))
-    for i in 1:Nq
-        if i == 1 #一番最初のループはおおもとになる行列の宣言をする(pauliを作る)
-            if index == 1 #1st qubitがXかYのとき
-                if pauli_name == "X"
-                    pauli = Xgate
-                elseif pauli_name == "Z"
-                    pauli = Zgate
-                else
-                    println("ERROR: undefined!")
-                    return False
-                end
-            else
-                pauli = Igate
-            end
-        else #2ループ目以降は変数pauliがあるはずなので、pauli \tensor (X or Z or I)を計算してpauliを上書き代入
-            if i == index #XかYを代入するタイミングのとき
-                if pauli_name == "X"
-                    pauli = kron(pauli, Xgate)
-                elseif pauli_name == "Z"
-                    pauli = kron(pauli, Zgate)
-                end
-            else
-                pauli = kron(pauli, Igate)
-            end
-        end
-    end
-    return pauli
-end
+
 
 Nq = 10 #qubit数
-N = 20  #乱数取る回数
+N = 30  #乱数取る回数
+Ave = 10 #平均を取る回数
 #β = 0  #最大10くらい
 H = make_hamiltonian(Nq)
 H_val, H_vec = eigen(H) 
@@ -151,8 +159,8 @@ B_list = [2,3,4,5,6,7,8,9,10]
 
 #-------------------------------------------------------------------------------------------
 #普通に実行する時
-TimeArray = zeros(Float64,N,N)
-for i in 1:1:N
+TimeArray = zeros(Float64,Ave,N)
+for i in 1:1:Ave
     for j in 1:N
         s = rand()*10
         if j == 1
@@ -169,14 +177,21 @@ size(TimeArray)
 out = open("time.txt","a")
 println(out,TimeArray)
 println(out,"")
-for i in 1:1:N
+for i in 1:1:Ave
     println(out,TimeArray[i,:])
 end
 println(out,"TimeMax")
-for i in 1:1:N
+for i in 1:1:Ave
     println(out,maximum(TimeArray[i,:]))
 end
 close(out)
+
+#乱数が入るタイミングなどのdataを保存
+filename = "ハミルトニアン(-有り)/ハイゼンベルク/data1.jld2"
+jldopen(filename,"w") do file
+    file["N"] = N
+    file["Time"] = TimeArray
+end
 
 function main()
     for β in β_list
@@ -186,20 +201,20 @@ function main()
             
             B = make_pauli(B_index,Nq,"X")
             println("β=",β,',',"A=1",',',"B=",B_index)
-            result = zeros(ComplexF64,N,T+1)
+            result = zeros(ComplexF64,Ave,T+1)
             result_ave = zeros(ComplexF64,T+1)
 
-            for i in 1:1:N #平均を取る回数
+            for i in 1:1:Ave #平均を取る回数
                 println(TimeArray[i,:])
                 println(out,TimeArray[i,:])
-                unitary_pool = make_unitary_pool(Nq,TimeArray[i,:])
+                unitary_pool = make_unitary_pool(Nq,TimeArray[i,:],N)
                 
                 for t in 0:1:T
                     println(t)
                     U = chose_unitary(unitary_pool,TimeArray[i,:],t)
                     OTOC = tr(ρ*U'*B'*U*A'*U'*B*U*A)
                     #println(out,OTOC)
-                    #println(OTOC)
+                    println(OTOC)
                     result[i,t+1] = OTOC
                 end
                 
@@ -210,14 +225,13 @@ function main()
                 println(out,"")
             end
             println(out,"average")
-            result_ave = result_ave ./ N
+            result_ave = result_ave ./ Ave
             for j in 1:1:T+1
                 println(out,result_ave[j])
             end
             println(out,"")
             println(out, "最後にRUが入ったタイミング: ",TimeMax)
             close(out)
-            
         end
     end
 end
@@ -225,43 +239,28 @@ end
 main()
 #-------------------------------------------------------------------------------------------------------------------
 #RUを追加する時
-ADD = 10 #増やすRUの数
-T_Old = 120 #前回のTimeMax
-TimeMaxArray = [
-90.75983339003538
-80.29388343940214
-92.82037216846993
-86.43967451581229
-96.19759379763019
-94.47052753880162
-93.41935705051624
-106.00603776249076
-98.08854350378054
-94.86913934276203
-87.10331300046053
-107.63802710982421
-102.09872979751607
-101.06617405814295
-95.71414128882643
-104.93657231735148
-102.80874474024715
-84.24460179738186
-83.1533822965697
-118.90930969918482]
+#dataをロード,filenameに注意
+filename = "ハミルトニアン(-有り)/ハイゼンベルク/data1.jld2"
+data = load(filename)
+jldopen(filename,"r") do file
+    N = file["N"]
+    TimeArray = file["Time"]
+end
+N_ADD = N + 10 #増やすRUの数
+T_Old = 100 #前回のTimeMax
 
 #RUを追加する時のRUが入る時間
-TimeAddArray = zeros(Float64,N,ADD)
-for i in 1:1:N
-    for j in 1:ADD
+TimeAddArray = zeros(Float64,Ave,N_ADD)
+for i in 1:1:Ave
+    for j in 1:N
+        TimeAddArray[i,j] = TimeArray[i,j]
+    end
+    for j in N+1:N_ADD
         s = rand()*10
-        if j == 1
-            TimeAddArray[i,j] = TimeMaxArray[i]+s
-        else
-            TimeAddArray[i,j] = TimeAddArray[i,j-1]+s
-        end
-        
+        TimeAddArray[i,j] = TimeAddArray[i,j-1]+s
     end
 end
+
 TimeAddArray
 TimeAddMax = maximum(TimeAddArray)
 T_New = Int(round(TimeAddMax) + 1)
@@ -269,11 +268,11 @@ size(TimeAddArray)
 out = open("time_Add.txt","a")
 print(out,TimeAddArray)
 println(out,"")
-for i in 1:1:N
+for i in 1:1:Ave
     println(out,TimeAddArray[i,:])
 end
 println(out,"")
-for i in 1:1:N
+for i in 1:1:Ave
     println(out,maximum(TimeAddArray[i,:]))
 end
 close(out)
@@ -286,13 +285,13 @@ function main_Add()
             
             B = make_pauli(B_index,Nq,"X")
             println("β=",β,',',"A=1",',',"B=",B_index)
-            result = zeros(ComplexF64,N,T_New-T_Old)
+            result = zeros(ComplexF64,Ave,T_New-T_Old)
             result_ave = zeros(ComplexF64,T_New-T_Old)
 
-            for i in 1:1:N #平均を取る回数
+            for i in 1:1:Ave #平均を取る回数
                 println(TimeAddArray[i,:])
                 println(out,TimeAddArray[i,:])
-                unitary_pool = make_unitary_pool(Nq,TimeAddArray[i,:])
+                unitary_pool = make_unitary_pool(Nq,TimeAddArray[i,:],N_ADD)
                 k = 1
                 for t in T_Old+1:1:T_New
                     println(t)
@@ -311,7 +310,7 @@ function main_Add()
                 println(out,"")
             end
             println(out,"average")
-            result_ave = result_ave ./ N
+            result_ave = result_ave ./ Ave
             for j in 1:1:T_New-T_Old
                 println(out,result_ave[j])
             end
@@ -323,6 +322,12 @@ function main_Add()
 end
 
 main_Add()
+
+filename2 = "ハミルトニアン(-有り)/ハイゼンベルク/data2.jld2"
+jldopen(filename2,"w") do file
+    file["N"] = N_ADD
+    file["Time"] = TimeAddArray
+end
 
 #=
 for i in 1:1:5
