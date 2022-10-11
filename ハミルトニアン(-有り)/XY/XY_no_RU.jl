b25aae751dff08cd9a85f805b64b21c7ffd24ba0
kutsuzawa-ryosuke <kutu.ryo1106@gmail.com>
2022-10-11 19:22:51 +0900

    XY RU無し結果

--- ハミルトニアン(-有り)/XY/XY_no_RU.jl
+++ ハミルトニアン(-有り)/XY/XY_no_RU.jl
@@ -110,16 +110,16 @@ for t in 0:1:5
 end
 close(out)
 =#
-A = make_pauli(4,Nq,"X")
-B_list = [5,8,10]
+A = make_pauli(3,Nq,"Z")
+B_list = [1,2,4,5,6,7,8,9,10]
 β_list = [0,1,3,5]
 for β in β_list
     ρ = exp(-1*β*H)/tr(exp(-β*H))
     for B_index in B_list
-        out = open("XY_no_RU.txt","a")
-        B = make_pauli(B_index,Nq,"Y")
-        println("β=",β,',',"A=4",',',"B=",B_index)
-        println(out,"β=",β,',',"A=4",',',"B=",B_index)
+        out = open("ハミルトニアン(-有り)/XY/パウリ(Z,X)/計算結果XY_no_RU3.txt","a")
+        B = make_pauli(B_index,Nq,"X")
+        println("β=",β,',',"A=3",',',"B=",B_index)
+        println(out,"β=",β,',',"A=3",',',"B=",B_index)
         for t in 0:1:200
             U = unitary(t)
             OTOC = tr(ρ*U'*B'*U*A'*U'*B*U*A)
