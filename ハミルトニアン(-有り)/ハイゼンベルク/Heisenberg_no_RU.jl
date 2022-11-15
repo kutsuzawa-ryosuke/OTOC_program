91a4ff31e26998da9ce51575af77bfbab2ecf1d5
kutsuzawa-ryosuke <kutu.ryo1106@gmail.com>
2022-11-16 08:12:19 +0900

    Heisenberg-XX-RU-no-result

--- ハミルトニアン(-有り)/ハイゼンベルク/Heisenberg_no_RU.jl
+++ ハミルトニアン(-有り)/ハイゼンベルク/Heisenberg_no_RU.jl
@@ -120,10 +120,10 @@ B_list = [2,3,4,5,6,7,8,9,10]
 for β in β_list
     ρ = exp(-1*β*H)/tr(exp(-β*H))
     for B_index in B_list
-        out = open("ハミルトニアン(-有り)/ハイゼンベルク/パウリ(X,Y)/計算結果/Heisenberg_no_RU.txt","a")
+        out = open("ハミルトニアン(-有り)/ハイゼンベルク/パウリ(X,X)/計算結果/Heisenberg_no_RU.txt","a")
         B = make_pauli(B_index,Nq,"X")
-        println("β=",β,',',"A=3",',',"B=",B_index)
-        println(out,"β=",β,',',"A=3",',',"B=",B_index)
+        println("β=",β,',',"A=1",',',"B=",B_index)
+        println(out,"β=",β,',',"A=1",',',"B=",B_index)
         for t in 0:1:200
             U = unitary(t)
             OTOC = tr(ρ*U'*B'*U*A'*U'*B*U*A)
